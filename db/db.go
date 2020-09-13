e93062e1374cd3c0bc6f1b72bd75b690527d7052
hoanguyen <httt.dev@gmail.com>
2020-09-13 08:11:26 +0700

    update user module / db connect

--- db/db.go
+++ db/db.go
@@ -1,1 +1,34 @@
 package db
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/gommon/log"
+	_ "github.com/lib/pq"
+
+)
+
+//SQL object keep connect to database + provide query function
+type SQL struct {
+	Db *sqlx.DB
+}
+// connect to postgres db
+func (s *SQL) Connect () {
+	dataSource := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"localhost",
+		"5432",
+		"postgres",
+		"Abc12345",
+		"timewise")
+
+	s.Db = sqlx.MustConnect("postgres", dataSource)
+	if err:= s.Db.Ping();err!=nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connect db ok")
+}
+
+//close connection
+func (s *SQL) Close()  {
+	s.Db.Close()
+}
